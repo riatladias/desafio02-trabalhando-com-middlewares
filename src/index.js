cfc6ca4d68bdf18b44b76591bafd1305afa5a1da
Riatla Dias <riatla.d@gmail.com>
2023-01-04 17:01:20 -0300

    Conclusão do desafio

--- src/index.js
+++ src/index.js
@@ -10,19 +10,56 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) return response.status(404).json({ error: "Username not found!" });
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro) return next();
+
+  if (!user.pro && user.todos.length < 10) return next();
+
+  return response.status(403).json({ error: "Todo reached the limit!" });
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) return response.status(404).json({ error: "Username not found!" });
+  if (!validate(id)) return response.status(400).json({ error: "ID is not uuid!" });
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) return response.status(404).json({ error: "Todo not found!" });
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) return response.status(404).json({ error: "Id not found!" });
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
